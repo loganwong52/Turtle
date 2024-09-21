cff8fe4f212c59d34a6bffc1d1598741835e309f
loganwong52 <loganwong52@gmail.com>
2024-09-21 09:52:00 -0400

    added fancy patterns

--- main.py
+++ main.py
@@ -12,30 +12,61 @@ def draw_hexagon(pen, colors):
         pen.left(60)
 
 
+def draw_2_hexagons(pen, colors):
+    draw_hexagon(pen, colors)
+    # Lift up the pen
+    pen.up()
+    pen.forward(150)
+    # Set the pen back down
+    pen.down()
+    draw_hexagon(pen, colors)
+
+
+def spiral(r, pen):
+    # Loop for printing spiral circle
+    for i in range(100):
+        pen.circle(r + i, 45)
+
+
 def main():
     # Create a Turtle screen
     screen = turtle.Screen()
 
     # create a Turtle object
     pen = turtle.Turtle()
+    # pen.tracer(False)
 
     # Set the speed of the turtle
-    pen.speed(5)
+    pen.speed("fastest")
 
     # define colors
     colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
-    draw_hexagon(pen, colors)
+    # draw_2_hexagons(pen, colors)
 
-    # Lift up the pen
+    spiral(10, pen)
     pen.up()
-
-    pen.forward(150)
-
-    # Set the pen back down
+    pen.right(45)
+    pen.backward(200)
+    pen.left(45)
     pen.down()
 
-    draw_hexagon(pen, colors)
+    r = 10
+    for i in range(r, 150, 2):
+        pen.circle(i)
+
+    pen.up()
+    pen.right(90)
+    pen.forward(150)
+    pen.left(90)
+    pen.down()
+
+    for s in range(3, 50):
+        i = s - 3
+        if s > len(colors):
+            i = s - (int(s / len(colors)) * len(colors))
+        pen.color(colors[i])
+        pen.circle(50, steps=s)
 
     # Close the turtle graphic window on click
     screen.exitonclick()
