20bcd791026386d152c279b022955f7748f39b09
loganwong52 <loganwong52@gmail.com>
2024-09-21 09:29:13 -0400

    added requirements.txt and imported turtle, but apparently its already installed!

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import turtle
+
+# Create a Turtle screen
+screen = turtle.Screen()
+
+# create a Turtle object
+pen = turtle.Turtle()
+
+# Set the speed of the turtle
+pen.speed(2)
+
+# define colors
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+for color in colors:
+    pen.color(color)
+    pen.forward(100)
+    pen.left(60)
+
 print("Hello world")
+
+# Close the turtle graphic window on click
+screen.exitonclick()
